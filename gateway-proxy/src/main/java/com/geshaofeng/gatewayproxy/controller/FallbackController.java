4369f7d11efef6a653a938431bc3577b3c99b76f
Gary ge <ge.sf.chn@gmail.com>
2019-02-27 20:23:47 +0800

    delete .idea

--- gateway-proxy/src/main/java/com/geshaofeng/gatewayproxy/controller/FallbackController.java
+++ gateway-proxy/src/main/java/com/geshaofeng/gatewayproxy/controller/FallbackController.java
@@ -4,9 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * TODO
  *
--- gateway-proxy/src/main/java/com/geshaofeng/gatewayproxy/filter/BodyDecoratorFilter.java
+++ gateway-proxy/src/main/java/com/geshaofeng/gatewayproxy/filter/BodyDecoratorFilter.java
@@ -4,19 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.support.BodyInserterContext;
-import org.springframework.cloud.gateway.support.CachedBodyOutputMessage;
-import org.springframework.cloud.gateway.support.DefaultServerRequest;
 import org.springframework.core.Ordered;
-import org.springframework.core.io.buffer.DataBuffer;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 /**
